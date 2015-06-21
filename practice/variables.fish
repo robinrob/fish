bab57c40c81c8141a41e3f241aa54895d47d9a5e
Robin Smith <robin@mrrobinsmith.com>
2015-06-21 21:00:20 +0100

    Need to continue porting

--- practice/variables.fish
+++ practice/variables.fish
@@ -1,6 +1,5 @@
 #!/usr/bin/env fish
 
-
 # Scalar
 set a 'one two three'
 echo $a
@@ -16,3 +15,8 @@ echo $ENV_VAR
 # Erase variable
 set -e ENV_VAR
 echo $ENV_VAR
+
+# -U sets a universal variable - value is shared across all current and future
+# fish sessions@
+set -U b 'i am universal'
+echo $b
